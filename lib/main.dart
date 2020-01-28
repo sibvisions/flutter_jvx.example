529b81ea77410ecbb2eb1915fcaac539649b642d
Lorenz Mueksch <lorenz.mueksch@sibvisions.com>
2020-01-28 15:56:30 +0100

    finally works

--- lib/main.dart
+++ lib/main.dart
@@ -8,16 +8,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Container(child: Image(image: AssetImage('assets/images/sib_visions.jpg', package: 'jvx_mobile_v3'),),);
     return JVxStartingWidget(
       config: Config(
-        baseUrl: 'http://127.0.0.1:8080/JVx.mobile/services/mobile',
+        baseUrl: 'http://192.168.1.42:8080/JVx.mobile/services/mobile',
         appName: 'demo',
         appMode: 'full',
         debug: true,
         username: 'features',
         password: 'features'
       ),
+      package: true,
     );
   }
 }
