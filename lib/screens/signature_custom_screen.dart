99db1ee3b2c95c43a887c916877703997ee52d45
Thomas Lehner <thomas.lehner@sibvisions.com>
2020-07-22 14:56:39 +0200

    person selection now possible in signature screen

--- lib/screens/signature_custom_screen.dart
+++ lib/screens/signature_custom_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:jvx_flutterclient/custom_screen/custom_screen.dart';
 import 'package:jvx_flutterclient/ui/screen/so_component_creator.dart';
+import 'package:jvx_flutterclient/ui/component/i_component.dart';
+import 'package:jvx_flutterclient/ui/container/co_panel.dart';
+import 'package:jvx_flutterclient/ui/component/co_custom_component.dart';
 import 'package:jvx_flutterclient/ui/screen/so_component_data.dart';
-
 import '../widgets/signature_custom_widget.dart';
 
 class SignatureCustomScreen extends CustomScreen {
@@ -16,7 +18,24 @@ class SignatureCustomScreen extends CustomScreen {
     SoComponentData data = this
         .componentScreen
         .getComponentData("JVxMobileDemo/Sig-3V/contacts#4");
-    return SignatureCustomWidget(componentData: data);
+    CoCustomComponent contactComp = new CoCustomComponent(
+      GlobalKey(debugLabel: 'contact'),
+      componentScreen.context,
+    );
+    contactComp.widget = SignatureCustomWidget(componentData: data);
+
+    CoPanel comp = this.componentScreen.getComponentFromName('signaturePanel');
+    this.componentScreen.replaceComponent(comp, contactComp);
+
+    IComponent component = this.componentScreen.getRootComponent();
+    if (component != null) {
+      return component.getWidget();
+    } else {
+      return Container(
+        alignment: Alignment.center,
+        child: Text('No root component defined'),
+      );
+    }
   }
 
   // if overriden, you have to handle server responses by your self or optionally call super.update to let the customScreen class do the work.
--- lib/widgets/signature_custom_widget.dart
+++ lib/widgets/signature_custom_widget.dart
@@ -30,53 +30,55 @@ class _SignatureCustomWidgetState extends State<SignatureCustomWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: <Widget>[
-          SizedBox(height: 10),
-          Text(
-            'Sign here:',
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-          Container(
-            margin: const EdgeInsets.all(15.0),
-            padding: const EdgeInsets.all(3.0),
-            decoration:
-                BoxDecoration(border: Border.all(color: UIData.ui_kit_color_2)),
-            child: Signature(
-              controller: _controller,
-              height: 200,
-              backgroundColor: Colors.white,
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      child: Center(
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 10),
+            Text(
+              'Sign here:',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
-          ),
-          //CLEAR CANVAS
-          Row(
-            children: <Widget>[
-              Expanded(
-                child: IconButton(
-                  icon: const Icon(Icons.save),
-                  color: UIData.ui_kit_color_2,
-                  onPressed: () async {
-                    if (_controller.isNotEmpty) {
-                      var data = await _controller.toPngBytes();
-                      selectRecord(0);
-                      setValues([base64Encode(data)]);
-                    }
-                  },
-                ),
+            Container(
+              margin: const EdgeInsets.all(15.0),
+              padding: const EdgeInsets.all(3.0),
+              decoration: BoxDecoration(
+                  border: Border.all(color: UIData.ui_kit_color_2)),
+              child: Signature(
+                controller: _controller,
+                height: 200,
+                backgroundColor: Colors.white,
               ),
-              Expanded(
-                child: IconButton(
-                  icon: const Icon(Icons.delete),
-                  color: UIData.ui_kit_color_2,
-                  onPressed: () {
-                    setState(() => _controller.clear());
-                  },
+            ),
+            //CLEAR CANVAS
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: IconButton(
+                    icon: const Icon(Icons.save),
+                    color: UIData.ui_kit_color_2,
+                    onPressed: () async {
+                      if (_controller.isNotEmpty) {
+                        var data = await _controller.toPngBytes();
+                        setValues([base64Encode(data)]);
+                      }
+                    },
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ],
+                Expanded(
+                  child: IconButton(
+                    icon: const Icon(Icons.delete),
+                    color: UIData.ui_kit_color_2,
+                    onPressed: () {
+                      setState(() => _controller.clear());
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
