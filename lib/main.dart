5ee38108fcecb194e6c17c2b8217dd4dbf5a0600
Lorenz Mueksch <lorenz.mueksch@sibvisions.com>
2020-12-21 14:16:25 +0100

    added custom config

--- lib/main.dart
+++ lib/main.dart
@@ -14,18 +14,24 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // To use the JVx flutter application you need to return the CustomApplicationWidget
-    // This manages all the Utils needed for the App to run.
-    return CustomApplicationWidget(
-      // Also if you want to run with a Developer Config you can pass one as a parameter
-      config: Config(
-          baseUrl: 'http://172.16.0.67:8080/JVx.mobile/services/mobile',
-          appName: 'demo',
-          debug: true,
-          username: 'features',
-          password: 'features'),
-      // To add a Custom Screen pass it as an parameter here:
-      screenManager: ExampleCustomScreenManager(),
+    return FutureBuilder(
+      future: Config.loadFile(path: 'env/dev.conf.json'),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          print(snapshot.data.properties['CUSTOM_PARAMETER']);
+
+          // To use the JVx flutter application you need to return the CustomApplicationWidget
+          // This manages all the Utils needed for the App to run.
+          return CustomApplicationWidget(
+            // Also if you want to run with a Developer Config you can pass one as a parameter
+            config: snapshot.data,
+            // To add a Custom Screen pass it as an parameter here:
+            screenManager: ExampleCustomScreenManager(),
+          );
+        } else {
+          return Container();
+        }
+      },
     );
   }
 }
