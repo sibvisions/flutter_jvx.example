084a00d2f02907f2943facf574bd25791cfed356
Bungeefan <severin.hamader@yahoo.de>
2023-03-02 11:41:12 +0100

    Upgraded to latest FlutterJVx

--- lib/main.dart
+++ lib/main.dart
@@ -6,27 +6,25 @@ import 'example_login.dart';
 void main() async {
   await FlutterUI.start(FlutterUI(
     appConfig: AppConfig(
-        title: "JVx Example",
-        uiConfig: const UiConfig(
-          showRememberMe: true,
-          rememberMeChecked: false,
+      title: "JVx Example",
+      uiConfig: const UiConfig(
+        showRememberMe: true,
+        rememberMeChecked: false,
+      ),
+      requestTimeout: const Duration(seconds: 10),
+      serverConfigs: [
+        ServerConfig(
+          baseUrl: Uri.parse("http://172.20.0.119:8888/JVx.mobile/services/mobile"),
+          appName: "demo",
+          username: "features",
+          password: "features",
         ),
-        requestTimeout: const Duration(seconds: 10),
-        serverConfigs: [
-          ServerConfig(
-            baseUrl: Uri.parse("http://172.20.0.119:8888/JVx.mobile/services/mobile"),
-            appName: "demo",
-            username: "features",
-            password: "features",
-          ),
-        ],
-        versionConfig: const VersionConfig(
-          commit: "070a55e2",
-          buildDate: "2021-09-26",
-        ),
-        startupParameters: {
-          "apiKey": "<insert token here>",
-        }),
+      ],
+      versionConfig: const VersionConfig(
+        commit: "070a55e2",
+        buildDate: "2021-09-26",
+      ),
+    ),
     appManager: ExampleCustomScreenManager(),
     loginBuilder: (context, mode) => ExampleLogin(mode: mode),
   ));
