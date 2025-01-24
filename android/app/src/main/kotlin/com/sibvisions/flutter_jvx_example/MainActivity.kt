ede833795ac779108714f4d30be9b2777c85e8b8
rjahn <>
2025-01-24 18:44:18 +0100

    fixed android build

--- android/app/src/main/kotlin/com/sibvisions/flutter_jvx_example/MainActivity.kt
+++ android/app/src/main/kotlin/com/sibvisions/flutter_jvx_example/MainActivity.kt
@@ -1,17 +1,7 @@
 package com.sibvisions.flutter_jvx_example
 
-import android.os.Build
-import android.os.Bundle
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            // Handle the splash screen transition.
-            val splashScreen = installSplashScreen()
-        }
-        super.onCreate(savedInstanceState)
-    }
 }
