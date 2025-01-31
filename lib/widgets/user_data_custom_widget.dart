be21d50864c695ec7850d89c1fb7e1532c3a7e53
rjahn <>
2025-01-31 17:25:31 +0100

    modern userdata page

--- lib/widgets/user_data_custom_widget.dart
+++ lib/widgets/user_data_custom_widget.dart
@@ -2,63 +2,197 @@ import 'package:flutter/material.dart';
 import 'package:flutter_jvx/flutter_jvx.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+
 class UserDataCustomWidget extends StatelessWidget {
   const UserDataCustomWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var profileImage = IConfigService().userInfo.value?.profileImage;
+    UserInfo? user = IConfigService().userInfo.value;
 
-    return Center(
-      child: Column(
+    return Scaffold(
+      body: Column(
         children: [
-          const SizedBox(
-            height: 20,
-          ),
-          CircleAvatar(
-            radius: 70,
-            backgroundImage: profileImage != null ? MemoryImage(profileImage) : null,
-            child: profileImage != null
-                ? null
-                : Icon(
-                    FontAwesomeIcons.userTie,
-                    color: Theme.of(context).colorScheme.primary,
-                    size: 60,
+          const Expanded(flex: 1, child: _TopPortion()),
+          Expanded(
+            flex: 3,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Text(
+                    _formatUserName(user),
+                    style: Theme.of(context)
+                        .textTheme.headlineSmall
+                        ?.copyWith(fontWeight: FontWeight.bold),
                   ),
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          Text(
-            "User: ${IConfigService().userInfo.value!.displayName!}",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
-              color: Theme.of(context).textTheme.bodyLarge!.color,
+                  const SizedBox(height: 5),
+                  Text("(${user?.roles.join(', ')})", style: Theme.of(context).textTheme.bodySmall),
+                  const SizedBox(height: 28),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      FloatingActionButton.extended(
+                        onPressed: () {},
+                        heroTag: 'follow',
+                        elevation: 1,
+                        shape: const StadiumBorder(),
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.blue,
+                        label: const Text("Follow"),
+                        icon: const Icon(Icons.person_add_alt_1),
+                      ),
+                      const SizedBox(width: 16.0),
+                      FloatingActionButton.extended(
+                        onPressed: () {},
+                        heroTag: 'message',
+                        elevation: 1,
+                        shape: const StadiumBorder(),
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.red,
+                        label: const Text("Message"),
+                        icon: const Icon(Icons.message_rounded),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                  const _ProfileInfoRow()
+                ],
+              ),
             ),
           ),
-          const Divider(
-            height: 20,
-            color: Colors.white,
-          ),
-          Text(
-            "Roles: ",
-            style: TextStyle(
-              color: Theme.of(context).textTheme.bodyLarge!.color,
-              fontSize: 20,
-            ),
-          ),
-          const SizedBox(height: 20),
-          Text(
-            IConfigService().userInfo.value!.roles.join(", "),
-            style: TextStyle(
-              color: Theme.of(context).textTheme.bodyLarge!.color,
-              fontSize: 16,
-            ),
-          ),
-          const Divider(height: 20, color: Colors.white),
         ],
       ),
     );
   }
+
+  String _formatUserName(UserInfo? info) {
+    return info?.displayName ?? IConfigService().username.value ?? "<unknown>";
+  }
+}
+
+class _TopPortion extends StatelessWidget {
+  const _TopPortion();
+
+  @override
+  Widget build(BuildContext context) {
+    UserInfo? user = IConfigService().userInfo.value;
+
+    var profileImage = user?.profileImage;
+
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        Container(
+          margin: const EdgeInsets.only(bottom: 65),
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Theme.of(context).colorScheme.primary, Theme.of(context).colorScheme.secondary]),
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(50),
+                bottomRight: Radius.circular(50),
+              )),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: SizedBox(
+            width: 150,
+            height: 150,
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                if (profileImage != null)
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: MemoryImage(profileImage)
+                          ),
+                    ),
+                  ),
+                if (profileImage == null)
+                  CircleAvatar(
+                    backgroundColor: Colors.grey.shade200,
+                    child: const Icon(FontAwesomeIcons.userTie, color: Colors.grey, size: 80),
+                  ),
+                Positioned(
+                  bottom: 0,
+                  right: 0,
+                  child: CircleAvatar(
+                    radius: 20,
+                    backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                    child: Container(
+                      margin: const EdgeInsets.all(8.0),
+                      decoration: const BoxDecoration(
+                          color: Colors.green, shape: BoxShape.circle),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class ProfileInfoItem {
+  final String title;
+  final int value;
+  const ProfileInfoItem(this.title, this.value);
+}
+
+class _ProfileInfoRow extends StatelessWidget {
+  const _ProfileInfoRow();
+
+  final List<ProfileInfoItem> _items = const [
+    ProfileInfoItem("Posts", 1191),
+    ProfileInfoItem("Followers", 309),
+    ProfileInfoItem("Following", 221),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80,
+      constraints: const BoxConstraints(maxWidth: 400),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: _items
+            .map((item) => Expanded(
+            child: Row(
+              children: [
+                if (_items.indexOf(item) != 0) const VerticalDivider(),
+                Expanded(child: _singleItem(context, item)),
+              ],
+            )))
+            .toList(),
+      ),
+    );
+  }
+
+  Widget _singleItem(BuildContext context, ProfileInfoItem item) => Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          item.value.toString(),
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        ),
+      ),
+      Text(
+        item.title,
+        style: Theme.of(context).textTheme.bodySmall,
+      )
+    ],
+  );
 }
